b4c045e66c84b29635478e28de5da15b64b6a72a
olivercarson-pubnub <oliver.carson@pubnub.com>
2022-07-15 12:54:04 -0700

    Update PubNubConnection with generic strings

--- getting-started/getting-started/PubNubConnection.swift
+++ getting-started/getting-started/PubNubConnection.swift
@@ -11,8 +11,8 @@ import Foundation
 import PubNub
 
 final class PubNubConnection {
-    private static let PUBNUB_PUBLISH_KEY = "pub-c-7ae1c68f-8009-470a-83d6-b14848591f3a"
-    private static let PUBNUB_SUBSCRIBE_KEY = "sub-c-a7cb2610-ad18-4a4a-a5db-188271558045"
+    private static let PUBNUB_PUBLISH_KEY = "myPublishKey"
+    private static let PUBNUB_SUBSCRIBE_KEY = "mySubscribeKey"
     static let shared : PubNub = PubNub(
         configuration: PubNubConfiguration(
           publishKey: PUBNUB_PUBLISH_KEY,
